84f042f47483d63de7b52888fb5bac38d5a88d17
Kevin <briand-kevin@hotmail.fr>
2023-07-24 17:35:16 +0200

    fix: update requirements
      - service file now automatically updated with current username
      - add luma.oled lib
      - add necessary libs for run luma.oled

--- INSTALL.sh
+++ INSTALL.sh
@@ -9,7 +9,7 @@ sudo apt-get install pigpiod -y
 echo -e "${GREEN}run pigpiod on startup${NC}"
 sudo systemctl enable pigpiod
 echo -e "${GREEN}install python${NC}"
-sudo apt-get install python3 python3-pip python3-venv -y
+sudo apt-get install python3 python3-pip python3-venv python3-pil libjpeg-dev zlib1g-dev libfreetype6-dev liblcms2-dev libopenjp2-7 libtiff5 -y
 echo -e "${GREEN}install dependencies${NC}"
 sudo python -m pip install -r requirements.txt
 if [ ! -f config.json ]
@@ -18,6 +18,7 @@ then
   cp config_template.json config.json
 fi
 echo -e "${GREEN}install deamon${NC}"
+sed -i "s/USER/$(whoami)/g" heatger.service
 sudo cp heatger.service /lib/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable heatger.service
