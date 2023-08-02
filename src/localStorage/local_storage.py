7a91b50e1e80292961205c8af645d57b9dcca3b6
Kevin <briand-kevin@hotmail.fr>
2023-08-02 11:18:04 +0200

    fix: sometimes an error occurred when reading the config file

--- src/localStorage/local_storage.py
+++ src/localStorage/local_storage.py
@@ -1,12 +1,28 @@
 """LocalStorage class"""
 import json
 import os
+import time
+from typing import Optional
+
 from src.localStorage.jsonEncoder.file_encoder import FileEncoder
 
 
 class LocalStorage:
     """Read/write json in file"""
+    _instance: Optional['LocalStorage'] = None
+    _initialized = False
+    _write = False
+
+    def __new__(cls, *args, **kwargs) -> 'LocalStorage':
+        if not isinstance(cls._instance, cls):
+            cls._instance = super(LocalStorage, cls).__new__(cls, *args, **kwargs)
+        return cls._instance
+
     def __init__(self, filename: str):
+        if self._initialized:
+            return
+
+        self.data = None
         self.filename = filename
         self.path = os.path.dirname(__file__).split('src')[0]
         try:
@@ -17,18 +33,26 @@ class LocalStorage:
 
     def read(self):
         """Reading data in file"""
+        while LocalStorage._write:
+            time.sleep(0.1)
+        if self.data is not None:
+            return json.loads(self.data)
+
         with open(self.path + self.filename, 'r', encoding='utf-8') as file:
             file.seek(0)
-            if file.readable():
-                json_data = file.read()
-                if json_data == '':
-                    return {}
-                return json.loads(json_data)
-        return {}
+            if not file.readable():
+                raise FileNotFoundError
+            json_data = file.read()
+            if json_data != '':
+                self.data = json_data
+        return json.loads(self.data)
 
     def write(self, data):
         """writing data in file"""
+        LocalStorage._write = True
         os.remove(self.path + self.filename)
         with open(self.path + self.filename, 'w', encoding='utf-8') as file:
             if file.writable():
                 file.write(json.dumps(data, indent=4, cls=FileEncoder))
+                self.data = json.dumps(data, indent=4, cls=FileEncoder)
+        LocalStorage._write = False
